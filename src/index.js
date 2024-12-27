ca1fdd4aea00037db53952bb37759aaf811551f8
jonah <jonahkf@gmail.com>
2024-12-27 20:46:39 +0900

    Added placeholder functions to index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ console.log("Battleship script loaded.");
 console.log("Welcome aboard captain. All systems online.");
 
 const gameController = () => {
+  // Global Variables
   const boardOne = document.querySelector(".board-one");
-  const boardTwo = document.querySelector(".board-one");
+  const boardTwo = document.querySelector(".board-two");
+
+  // Global Functions
+  const startGame = () => {
+    // Ask if P2 is human using popups
+    const human = false; // temp
+    const playerOne = new Player(true);
+    const playerTwo = new Player(human);
+  };
 
   const updateScreens = () => {};
 
-  const renderBoard = (board) => {};
+  const renderBoard = (player, board) => {};
 
   const setShip = (ship) => {};
+
+  // Game Start
+  startGame();
 };
+
+gameController();
