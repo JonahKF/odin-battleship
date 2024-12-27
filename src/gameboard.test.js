8774e96251df6a24a9a277886130aba12765f0de
jonah <jonahkf@gmail.com>
2024-12-27 20:46:39 +0900

    Added: Tests for Player class, import GameController from index.js

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,5 +1,7 @@
 import { Ship, Gameboard, Player } from "./gameboard.js";
+import { GameController } from "./index.js";
 
+// Ship Test Suite
 test("tests Ship existing", () => {
   expect(typeof new Ship("carrier")).toEqual("object");
 });
@@ -12,6 +14,7 @@ test("tests Ship lengths", () => {
   expect(new Ship("patrolboat").length).toEqual(2);
 });
 
+// Gameboard Test Suite
 test("tests Gameboard existing", () => {
   expect(typeof new Gameboard()).toEqual("object");
 });
@@ -29,4 +32,22 @@ test("tests ship placement", () => {
   expect(board.board[0][1].type).toEqual("carrier");
   expect(board.board[0][4].type).toEqual("carrier");
   expect(board.board[0][5]).toEqual(null);
+});
+
+// Player Test Suite
+test("tests ship placement within a Player gameboard", () => {
+  const testPlayer = new Player();
+  expect(testPlayer.human).toBe(false);
+
+  const carrier = new Ship("carrier");
+  const submarine = new Ship("submarine");
+
+  testPlayer.gameboard.placeShip(carrier, [0, 0], false);
+  testPlayer.gameboard.placeShip(submarine, [2, 3], true);
+
+  expect(typeof testPlayer.gameboard.board[0][0]).toEqual("object");
+  expect(testPlayer.gameboard.board[0][0].type).toEqual("carrier");
+  expect(testPlayer.gameboard.board[0][1].type).toEqual("carrier");
+  expect(testPlayer.gameboard.board[0][4].type).toEqual("carrier");
+  expect(testPlayer.gameboard.board[0][5]).toEqual(null);
 });
