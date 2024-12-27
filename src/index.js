d4cf924d5261b04a27a9cd4c9d210eadbdeb2983
jonah <jonahkf@gmail.com>
2024-12-27 20:46:39 +0900

    Added: Skeleton functions in gameController, board classes in HTML

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@ import { Ship, Gameboard, Player } from "./gameboard.js";
 console.log("Battleship script loaded.");
 console.log("Welcome aboard captain. All systems online.");
 
-const gameController = () => {};
+const gameController = () => {
+  const boardOne = document.querySelector(".board-one");
+  const boardTwo = document.querySelector(".board-one");
+
+  const updateScreens = () => {};
+
+  const renderBoard = (board) => {};
+
+  const setShip = (ship) => {};
+};
